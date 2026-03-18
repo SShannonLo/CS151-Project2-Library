0c5cc240193dea1a22970d62fd852c1f1d20e0a9
angelinary <angelinaryabechenkova@gmail.com>
2026-03-17 23:20:54 -0700

    Added getters and Setters and the rest of the method sceletons from the EC file we submitted last week

--- Library.java
+++ Library.java
@@ -12,6 +12,32 @@ public class Library {
             this.membershipType = membershipType;
         }
 
+        // Getters 
+        public String getName() {
+            return name;
+        }
+        
+        public boolean getIsActive() {
+            return isActive;
+        }
+
+        public String getMembershipType() {
+            return membershipType;
+        }
+        
+        // Setters
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public void setIsActive(boolean isActive) {
+            this.isActive = isActive;
+        }
+
+        public void setMembershipType(String membershipType) {
+            this.membershipType = membershipType;
+        }
+        
         // Returns a boolean value if the membership is active or not
         public boolean activeMember() {
             return isActive;
@@ -31,5 +57,21 @@ public class Library {
             }
         }
 
+        // Returns the information about which books have been loaned and which are past due
+        public void bookActivity(String bookTitle) {
+            System.out.println(name + " borrowed: " + bookTitle);
+        }
+
+        // Return a book
+        public void bookActivity(String bookTitle, boolean returning) {
+            if (returning) {
+                System.out.println(name + " returned: " + bookTitle);
+            }
+        }
+
+        // View activity
+        public void bookActivity() {
+            System.out.println(name + " checked their book activity.");
+        }
     }
 }
