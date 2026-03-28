639c9d7b10f329337bcb6d890f927893416cda85
Yanin Arevalos <yanin.arevalos@sjsu.edu>
2026-03-27 18:26:41 -0700

    add MemberNotFoundException exception

--- MemberNotFoundException.java
+++ MemberNotFoundException.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
 
-public class MemberNotFoundException {
+// thrown when a member cannot be found in the library
+public class MemberNotFoundException extends Exception {
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
 }
