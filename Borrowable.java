09f4bbe9c6a153abe2e06aedf7703f32965bfe5f
Keyi859 <kelly326859@gmail.com>
2026-03-27 22:13:53 -0700

    Update Borrowable interface methods
    Commented out the reserve method and added reserveBook method.

--- Borrowable.java
+++ Borrowable.java
@@ -3,6 +3,6 @@ public interface Borrowable{
     boolean checkOut(String memberId);
     boolean returnBook();
     void markOverDue();
-    boolean reserve(String memberId);
+    //boolean reserve(String memberId);
 	boolean reserveBook(String memberId);
 }
